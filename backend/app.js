e8ab0dd311c8a47ec33384b709f7ee440e337d43
KevalBhavsar05 <18kevalbhavsar@gmail.com>
2025-06-02 17:19:18 +0530

    --

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,6 @@ const permissionRoutes = require("./routes/permissionRoutes");
 const sceRoutes = require("../backend/routes/sceRoutes");
 const adminRoutes = require("../backend/routes/adminRoutes");
 const logbookFormRoutes = require("../backend/routes/logbookFormRoutes");
-const registerRoutes = require("./routes/registerRoutes");
 const searchableLog = require("./routes/searchableLogsRoutes")
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -26,7 +25,6 @@ app.set("views", path.join(__dirname, "../website/views"));
 app.use(express.static(path.join(__dirname, "../website/public")));
 
 // Routes
-app.use("/logRegister", registerRoutes);
 app.use("/", authRoutes);
 app.use("/", searchableLog);
 app.use("/", permissionRoutes);
